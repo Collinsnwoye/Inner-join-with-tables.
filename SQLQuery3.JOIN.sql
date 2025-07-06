66dc67943e84ef62579353f6a7c2ae27559bc468
Collinsnwoye <160544205+Collinsnwoye@users.noreply.github.com>
2025-07-06 02:42:02 +0100

    Update SQLQuery3.JOIN.sql

--- SQLQuery3.JOIN.sql
+++ SQLQuery3.JOIN.sql
@@ -7,10 +7,20 @@ SELECT
   t.client_id, 
   t.driver_id, 
   c.users_id AS client_user_id,
-  d.users_id AS driver_user_id
+  d.users_id AS driver_user_id,
+   t.request_at AS Day
 FROM Trip t
 INNER JOIN Users c
 ON t.client_id = c.users_id AND c.banned = 'NO' AND c.role = 'client'
 INNER JOIN Users d
 ON t.driver_id = d.users_id AND d,banned = 'NO' AND d.role = 'driver'
-
+  ROUND(
+    SUM(CASE 
+          WHEN t.status IN ('cancelled_by_driver', 'cancelled_by_client') THEN 1 
+          ELSE 0 
+        END) * 1.0 / COUNT(*),
+    2
+  ) AS `Cancellation Rate`
+WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at
+ORDER BY t.request_at;
