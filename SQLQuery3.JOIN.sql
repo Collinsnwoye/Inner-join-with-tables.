bee1f0651e64b33bafdcbd7b7a528ec884eca05a
Collinsnwoye <160544205+Collinsnwoye@users.noreply.github.com>
2025-07-06 02:30:15 +0100

    Add files via upload
    An IINNER JOIN task.

---
+++ SQLQuery3.JOIN.sql
@@ -0,0 +1,16 @@
+SELECT M.First_Name, S.Last_Name 
+FROM MathematicsStudent M
+INNER JOIN School S
+ON M.First_Name = S.First_Name;
+
+SELECT 
+  t.client_id, 
+  t.driver_id, 
+  c.users_id AS client_user_id,
+  d.users_id AS driver_user_id
+FROM Trip t
+INNER JOIN Users c
+ON t.client_id = c.users_id AND c.banned = 'NO'
+INNER JOIN Users d
+ON t.driver_id = d.users_id AND d,banned = 'NO'
+
